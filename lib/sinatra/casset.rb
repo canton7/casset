aca53355b5b93e8ac2116a94ef9c82116e3a0567
Antony Male <antony.male@gmail.com>
2012-01-17 13:56:02 +0000

    Second attempt at Sinatra adapter -- better config syntax

--- lib/sinatra/casset.rb
+++ lib/sinatra/casset.rb
@@ -2,31 +2,34 @@ require 'casset'
 
 module Sinatra
 	module Casset
-		module CassetConfig
-			extend self
+		class CassetConfig
 
-			def parse(&blk)
-				@options = {}
+			attr_reader :funcs, :configuration
+
+			def initialize(&blk)
+				@funcs, @configuration = {}, {}
+
 				parameter :add_assets, :add_group, :group_options, :add_namespace,
 					:set_default_namespace, :js, :css, :enable, :disable, :add_parser,
 					:set_minifier, :clear_cache
+
+					parse_funcs(&blk) if block_given?
+			end
+
+			def parse_funcs(&blk)
 				blk.arity <= 0 ? instance_eval(&blk) : yield(self) if block_given?
-				@options
+				@funcs
+			end
+
+			def config(config)
+				@configuration.merge!(config)
 			end
 
 			def parameter(*names)
 				names.each do |name|
-					define_method name do |*values, &blk|
-						#puts "FUNCTION CALLED #{name}"
-						@options[name] = {:args => [*values], :block => blk}
-						#pp @options
+					self.class.send(:define_method, name) do |*values, &blk|
+						@funcs[name] = {:args => [*values], :block => blk}
 					end
-					# TODO this doesn't work
-					#define_method "#{name}=" do |*values|
-					#	puts "FUNCTION SETTER #{name}"
-					#	@options[name] = values
-					#end
-					#alias_method :"#{name}=", name
 				end
 			end
 		end
@@ -34,21 +37,16 @@ module Sinatra
 		module CassetConfigurator
 			extend self
 
-			def create(config)
+			def create(config_obj)
 				casset = ::Casset::Casset.new
-				config(config, casset)
+				config(casset, :funcs => config_obj.funcs, :config => config_obj.configuration)
 			end
 
-			def config(config, casset)
-				options = {}
-				config.each do |key, value|
-					if casset.respond_to?(key)
-						casset.send(key, *value[:args], &value[:block])
-					else
-						options[key] = value[:args].first
-					end
+			def config(casset, opts)
+				casset.config(opts[:config])
+				opts[:funcs].each do |key, value|
+					casset.send(key, *value[:args], &value[:block])
 				end
-				casset.config(options) unless options.empty?
 				casset
 			end
 		end
@@ -56,15 +54,18 @@ module Sinatra
 		module Helpers
 			def assets(&blk)
 				if block_given?
-					config = CassetConfig.parse(&blk)
-					CassetConfigurator.config(config, settings.casset)
+					funcs = CassetConfig.new(&blk).funcs
+					CassetConfigurator.config(settings.casset, :funcs => funcs)
 				end
 				settings.casset
 			end
 		end
 
 		def assets(&blk)
-			set :casset_options, CassetConfig.parse(&blk)
+			config = settings.casset_options
+			config.parse_funcs(&blk)
+			set :casset_options, config
+			config
 		end
 
 		def self.registered(app)
@@ -72,6 +73,7 @@ module Sinatra
 			app.before do
 				app.set :casset, CassetConfigurator.create(app.settings.casset_options)
 			end
+			app.set :casset_options, CassetConfig.new
 		end
 	end
 	register Casset
